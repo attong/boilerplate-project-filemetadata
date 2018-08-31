dd439908438ec1dde933de2e87a95affe7990426
attong <attong@berkeley.edu>
2018-08-30 23:54:47 -0700

    implemented /api/fileanalyse. uses multer to upload file and returns file name and size in bytes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var cors = require('cors');
 var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 // require and use "multer"...
 
 var app = express();
@@ -13,6 +14,11 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req,res)=>{
+	// res.send(req.file);
+	res.json({"filename": req.file.originalname,"type": req.file.mimetype, "size": req.file.size});
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
